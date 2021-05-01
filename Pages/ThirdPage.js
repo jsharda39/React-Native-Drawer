2a90dd86a393aa83d7d6c37128d51519e10832fe
jsharda39 <jshrda39@gmail.com>
2021-05-01 13:30:31 +0530

    initial commit

---
+++ Pages/ThirdPage.js
@@ -0,0 +1,40 @@
+// Custom Navigation Drawer / Sidebar with Image and Icon in Menu Options
+// https://aboutreact.com/custom-navigation-drawer-sidebar-with-image-and-icon-in-menu-options/
+
+import * as React from 'react';
+import {Button, View, Text, SafeAreaView} from 'react-native';
+
+const ThirdPage = ({navigation}) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16,
+            }}>
+            This is Third Page under Second Page Option
+          </Text>
+          <Button
+            onPress={() => navigation.navigate('FirstPage')}
+            title="Go to First Page"
+          />
+          <Button
+            onPress={() => navigation.navigate('SecondPage')}
+            title="Go to Second Page"
+          />
+        </View>
+       
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ThirdPage;
